311f50e0ad74f392f8de30512391d35d906230c5
Matheus Monteiro <59981510+monteurmatt@users.noreply.github.com>
2025-05-22 12:17:47 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Instalar o RPM Fusion
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Instalar o driver da NVIDIA e CUDA (Descomentar caso placa nvidia)
+#sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs -y
+#sudo dnf install nvidia-vaapi-driver -y
+
+# Corrigir os problemas de codec
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+sudo dnf groupupdate sound-and-video -y
+sudo dnf install amrnb amrwb faad2 flac gpac-libs lame libde265 libfc14audiodecoder mencoder x264 x265 ffmpegthumbnailer -y
+
+# Instalar o GNOME Tweaks para configurar o botão de minimizar
+sudo dnf install gnome-tweaks -y
+
+# Instalar o Google Chrome (e remover o aviso de gerenciado pela organização)
+sudo dnf install fedora-workstation-repositories -y
+sudo dnf config-manager --set-enabled google-chrome
+sudo dnf install google-chrome-stable -y
+sudo dnf remove fedora-chromium-config -y
+
+# GIMP e Krita 
+sudo dnf install gimp krita -y
+
+# Docker e Docker Compose (CLI e suporte via podman)
+sudo dnf install docker docker-compose podman-docker -y
+sudo systemctl enable --now docker
+sudo usermod -aG docker $USER
+
+# Python3 e pip (pip geralmente vem com python3, mas garante que esteja instalado)
+sudo dnf install python3 python3-pip -y
+
+# Instalar utilitários para terminal
+sudo dnf install ranger speedtest-cli btop -y
+
+# Instalar as fontes da Microsoft
+sudo dnf install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -y
+
+# Instalar ferramentas para jogos
+sudo dnf install steam -y
+flatpak install flathub com.vysp3r.ProtonPlus
+flatpak install flathub com.steamgriddb.steam-rom-manager
+flatpak install flathub com.steamgriddb.SGDBoop
+flatpak install flathub info.cemu.Cemu
+flatpak install flathub com.usebottles.bottles
+flatpak install flathub com.heroicgameslauncher.hgl
+flatpak install flathub dev.lizardbyte.app.Sunshine
+
+# Instalar aplicativos em flatpak
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.spotify.Client
+flatpak install flathub com.obsproject.Studio
+flatpak install flathub io.github.celluloid_player.Celluloid
+flatpak install flathub org.gnome.Boxes
+flatpak install flathub com.mattjakeman.ExtensionManager
+flatpak install flathub com.github.tchx84.Flatseal
+flatpak install flathub org.nickvision.tubeconverter
+flatpak install flathub org.localsend.localsend_app
+flatpak install flathub page.codeberg.libre_menu_editor.LibreMenuEditor
+flatpak install flathub de.haeckerfelix.Fragments
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.visualstudio.code
+flatpak install flathub net.davidotek.pupgui2
+
+
+# Instalar e habilitar no ExtensionManager a bandeja de apps em 2 plano e barra inferior fixa
+sudo dnf install gnome-shell-extension-appindicator gnome-shell-extension-dash-to-dock -y
+gnome-extensions enable dash-to-dock@micxgx.gmail.com
+gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+gnome-extensions enable just-perfection-desktop@just-perfection
+
+# Instalar e habilitar via extensions-cli o 'Start Overlay in Application View' (Super Key para menu de apps)
+sudo dnf install gnome-extensions-cli -y
+gext install 5040
+gext enable start-overlay-in-application-view@hex_cz
+
+
+
+# Instalar as fontes que estão na pasta: Fontes
+# Aplicativos para instalar depois manualmente: DaVinci Resolve, Figma for Desktop, Insync
+# Ajustar os problemas do DaVinci Resolve segundo esse tutorial: https://github.com/H3rz3n/Davinci-Resolve-Fedora-38-39-40-Fix
